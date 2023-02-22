910e0bef338640db883c989bd38a1ca34c79a102
Chawla <703331146@genpact.com>
2023-02-22 14:13:37 +0530

    fix for cors issue

--- src/main/java/com/genpact/insurance/CorsFilter.java
+++ src/main/java/com/genpact/insurance/CorsFilter.java
@@ -15,6 +15,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 	public class CorsFilter implements WebMvcConfigurer {
 		@Override
 		public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
+			registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*"); 
 		}
 	}
